aa10405fb55292dd9e07f4b26ee9ec14714868f8
mkm1997 <mmanish184@gmail.com>
2018-02-07 17:46:08 +0530

    intial commit

---
+++ app/src/main/java/com/pythonanywhere/mkmtutorials/game/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.pythonanywhere.mkmtutorials.game
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}

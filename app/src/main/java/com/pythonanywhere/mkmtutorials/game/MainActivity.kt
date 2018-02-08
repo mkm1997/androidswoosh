c1dcb113cebfb548d43bdbb8a86b9b915b3bf311
mkm1997 <mmanish184@gmail.com>
2018-02-09 00:37:32 +0530

    intent button

--- app/src/main/java/com/pythonanywhere/mkmtutorials/game/MainActivity.kt
+++ app/src/main/java/com/pythonanywhere/mkmtutorials/game/MainActivity.kt
@@ -1,12 +1,25 @@
 package com.pythonanywhere.mkmtutorials.game
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+        getstarted_button.setOnClickListener {
+
+
+
+            val getintent = Intent(this,Main2Activity::class.java)  //intent is use for make call the other function
+                                                                                        //of the app like calling for open camera caaling for broweser
+                                                                                //this refer the context of the same app
+            startActivity(getintent)
+        }
     }
 }
